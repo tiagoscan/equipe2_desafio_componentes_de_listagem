7504806b87041221cf8b2998b4ba7038e12a8f4d
Tiago Candido <tiagocan@gmail.com>
2025-09-09 20:03:24 -0300

    feat: criação layout da tela perfil usuario

--- app/src/main/java/com/mentoriaandroid/myapplication/PerfilUsuarioActivity.kt
+++ app/src/main/java/com/mentoriaandroid/myapplication/PerfilUsuarioActivity.kt
@@ -1,20 +1,37 @@
 package com.mentoriaandroid.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mentoriaandroid.myapplication.databinding.ActivityPerfilUsuarioBinding
 
 class PerfilUsuarioActivity : AppCompatActivity() {
+
+private val binding by lazy {
+    ActivityPerfilUsuarioBinding.inflate(layoutInflater)
+}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_perfil_usuario)
+        setContentView(binding.root)
+        /*setContentView(R.layout.activity_perfil_usuario)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+
+        binding.cardViewConversas.setOnClickListener {
+            startActivity(
+                Intent(this, InicialRestaurantesActivity::class.java)
+            )
         }
+
     }
 }
